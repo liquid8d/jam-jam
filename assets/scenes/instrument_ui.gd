8e2714e3caf3fbb5af7a2dfa3d212d78c016f555
liquid8d <liquid8d@gmail.com>
2024-07-13 19:58:21 -0400

    ui updates  - updated styles and UI button prefab  - updated credits

--- assets/scenes/instrument_ui.gd
+++ assets/scenes/instrument_ui.gd
@@ -1,6 +1,8 @@
 @tool
 extends Panel
 
+const UI_BUTTON = preload("res://assets/scenes/ui_button.tscn")
+
 @onready var grid_container: GridContainer = %GridContainer
 @onready var label: Label = %Label
 
@@ -21,10 +23,11 @@ func update_presets():
 
 	for item in JamJam.presets:
 		if item.instrument == instrument:
-			var btn = Button.new()
-			btn.custom_minimum_size = Vector2(128, 96)
+			var btn = UI_BUTTON.instantiate()
 			btn.name = item.name
 			btn.text = item.label
+			if item.has("type"):
+				btn.get_node("Type").text = item.type
 			btn.pressed.connect(_on_voice_select.bindv([item.name]))
 			grid_container.add_child(btn)
 			btn.owner = self
