d00a4d64dbcc481a2ffb66f954a13c7f79a7340e
liquid8d <liquid8d@gmail.com>
2024-07-14 17:22:48 -0400

    use press to hold

--- assets/scenes/plushie_pet.gd
+++ assets/scenes/plushie_pet.gd
@@ -1,6 +1,5 @@
 extends Area3D
 
-@onready var animation_player: AnimationPlayer = $"../godot_plushie_animated/AnimationPlayer"
 @onready var shaker_component_3d: ShakerComponent3D = $ShakerComponent3D
 
 func _on_body_entered(body: Node3D) -> void:
